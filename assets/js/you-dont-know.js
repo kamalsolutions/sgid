5d1381a5c205739fbcc4b15d2f5d7e8a04461e36
Kofi Amoussou <karsoft92@gmail.com>
2018-02-14 17:59:16 +0000

    init

---
+++ assets/js/you-dont-know.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+
+  /* Hide mobile menu after clicking on a link
+    -----------------------------------------------*/
+    $('.navbar-collapse a').click(function(){
+        $(".navbar-collapse").collapse('hide');
+    });
+})
